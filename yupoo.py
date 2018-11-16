1522e2df5489077e2da942fab9f108b67d1df28d
Wendyluo0625 <Wendyluo1990@163.com>
2018-11-16 14:28:23 +0800

    181116

--- yupoo.py
+++ yupoo.py
@@ -9,19 +9,41 @@ def getHtml(url):
     html = page.read()
     return html
 
+def getUrl(html):
+  #  ureg = r'href=\/photos\/"(.*?)"\/uid=1'
+    ureg = r'href="(\/photos\/.*\?uid=1)"'
+    treg = r'text_overflow album__title\">(.+i?)</div>'
+    urlre = re.compile(ureg)
+    titlere = re.compile(treg)
+    urllist = re.findall(urlre, html)
+    titlelist = re.findall(titlere, html)
+    return titlelist,urllist
+
 def getDes(html):
     pass
 
-def getImg(html):
+def getImg(url,title):
     reg = r'data-origin-src="(.*?\.jpg)"'
     imgre = re.compile(reg)
-    imglist = re.findall(imgre, html)
+    imglist = re.findall(imgre, url)
+    #create seprate folder
+    path = title
+    #os.makedirs(path)
     x = 0
     for imgurl in imglist:
         imgurl = 'http:'+imgurl
         urllib.urlretrieve(imgurl, "%s.jpg" %x)
         x+=1
 
-html =  getHtml("http://x.yupoo.com/photos/zoestreet/albums/39807974?uid=1")
-print(getImg(html))
+url=getHtml("http://x.yupoo.com/photos/zoestreet/albums?tab=gallery")
+t_u = getUrl(url)
+
+l = 0 
+for t in t_u:
+    #getImg(urls[l],titles[l])
+    print(t)
+    l+=0
+
+#print(len(getUrl(url)[0]))
+#print(len(getUrl(url)[1]))
 
